a9ba1db5d6b190d17c894f74fab45a6925915870
iurii marinin <marininpost@gmail.com>
2023-07-03 15:32:11 +0400

    change makefile, add option type

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,7 +3,10 @@ import { program } from 'commander';
 
 program
   .version('1.0.0')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
   .description('Compares two configuration files and shows a difference.')
-  .helpOption('-h, --help', 'output usage information');
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format <type>', 'output format');
 
 program.parse();

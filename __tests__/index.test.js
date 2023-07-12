fcae3cef5f1d8ee9dd0b4e268d9edff8a00ac573
iurii marinin <marininpost@gmail.com>
2023-07-12 09:25:13 +0400

    add test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,7 +1,8 @@
 import fs from 'fs';
+import path from 'path';
 import genDiff from '../src/index.js';
 
 test('gendiff', () => {
-  expect(genDiff('../__fixtures__/file1.json', '../__fixtures__/file2.json'))
-    .toEqual(fs.readFileSync('./__fixtures__/expeted_file_stylish.txt', 'utf8'));
+  expect(genDiff(path.join('.', '__fixtures__', 'file1.json'), path.join('.', '__fixtures__', 'file2.json')))
+    .toEqual(fs.readFileSync(path.join('.', '__fixtures__', 'expected_file_stylish.txt'), 'utf8'));
 });

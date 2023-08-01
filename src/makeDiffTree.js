7beddd65df79c6547e8fd9644d28cbd17085d3b8
iurii marinin <marininpost@gmail.com>
2023-08-01 10:01:48 +0400

    change makeDiffTree

--- src/makeDiffTree.js
+++ src/makeDiffTree.js
@@ -6,19 +6,20 @@ const makeDiffTree = (obj1, obj2) => {
   const unionKeys = getUnionKeys(obj1, obj2);
   return unionKeys.reduce((acc, key) => {
     if (_.isObject(obj1[key]) && _.isObject(obj2[key])) {
-      acc.push({ key, value: makeDiffTree(obj1[key], obj2[key]), status: 'nested' });
-    } else if (!_.has(obj1, key)) {
-      acc.push({ key, value: obj2[key], status: 'added' });
-    } else if (!_.has(obj2, key)) {
-      acc.push({ key, value: obj1[key], status: 'deleted' });
-    } else if (obj1[key] === obj2[key]) {
-      acc.push({ key, value: obj1[key], status: 'unchanged' });
-    } else {
-      acc.push({
-        key, oldValue: obj1[key], newValue: obj2[key], status: 'changed',
-      });
+      return [...acc, { key, value: makeDiffTree(obj1[key], obj2[key]), status: 'nested' }];
     }
-    return acc;
+    if (!_.has(obj1, key)) {
+      return [...acc, { key, value: obj2[key], status: 'added' }];
+    }
+    if (!_.has(obj2, key)) {
+      return [...acc, { key, value: obj1[key], status: 'deleted' }];
+    }
+    if (obj1[key] === obj2[key]) {
+      return [...acc, { key, value: obj1[key], status: 'unchanged' }];
+    }
+    return [...acc, {
+      key, oldValue: obj1[key], newValue: obj2[key], status: 'changed',
+    }];
   }, []);
 };
 

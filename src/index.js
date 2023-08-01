52a7a13e23630a3909872282d3d477108c0092ae
iurii marinin <marininpost@gmail.com>
2023-08-01 09:50:06 +0400

    change index

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ import formatDefin from './formaters/index.js';
 
 const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
   const format = path.extname(filepath1);
-  const makeObject1 = parsers(fs.readFileSync(path.resolve(process.cwd(), filepath1)), format);
-  const makeObject2 = parsers(fs.readFileSync(path.resolve(process.cwd(), filepath2)), format);
+  const makeObject1 = parsers(fs.readFileSync(path.resolve(process.cwd(), filepath1), 'utf-8'), format);
+  const makeObject2 = parsers(fs.readFileSync(path.resolve(process.cwd(), filepath2), 'utf-8'), format);
   const tree = makeDiffTree(makeObject1, makeObject2);
   return formatDefin(tree, formatName);
 };
